b71ea421d8b7d9ead9498c0ea8b5dcaeee05d254
sadegh <sadeghte@gmail.com>
2019-04-15 12:00:02 +0430

    minor update

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <div style="max-width: 40em; margin: auto">
+      <h1>You are not logged in</h1>
+      <p>this page needs to be logged in</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'coreui-no-sidemenu',
+    mounted: function() {
+      if(this.$auth.loggedIn)
+        window.location.href = '/';
+    }
+  }
+</script>
